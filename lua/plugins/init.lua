a955eaccefeb779b423c2c0858c8321a28827f9c
Arnesh Roychowdhury <84434238+ArneshRC@users.noreply.github.com>
2025-04-04 15:25:09 +0530

    plugins(surround): changed mappings

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -244,15 +244,17 @@ local plugins = {
         config = function()
             require('nvim-surround').setup {
                 surrounds = {
-                    ['w'] = {
-                        add = { [[\textbf{]], [[}]] },
-                        change = function() end,
-                        delete = function() end
+                    ["c"] = {
+                        add = function()
+                            local cmd = require("nvim-surround.config").get_input "Command: "
+                            return { { "\\" .. cmd .. "{" }, { "}" } }
+                        end,
                     },
-                    ['e'] = {
-                        add = { [[\textit{]], [[}]] },
-                        change = function() end,
-                        delete = function() end
+                    ["e"] = {
+                        add = function()
+                            local env = require("nvim-surround.config").get_input "Environment: "
+                            return { { "\\begin{" .. env .. "}" }, { "\\end{" .. env .. "}" } }
+                        end,
                     },
                 }
             }
